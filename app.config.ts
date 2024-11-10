8fc3ca13971d5b6d8852fe76cbc97e6f76f32982
alakhpc <36898190+alakhpc@users.noreply.github.com>
2024-11-09 20:47:43 -0500

    external

--- app.config.ts
+++ app.config.ts
@@ -4,6 +4,9 @@ import viteTsConfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   server: {
     preset: "cloudflare-pages",
+    rollupConfig: {
+      external: ["node:async_hooks"],
+    },
   },
   vite: {
     plugins: [viteTsConfigPaths({ projects: ["./tsconfig.json"] })],
