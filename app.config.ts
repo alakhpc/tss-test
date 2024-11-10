84b5bfd61e19d7fb970ce52954a31ec19d5a625a
alakhpc <36898190+alakhpc@users.noreply.github.com>
2024-11-09 20:45:45 -0500

    cf pages

--- app.config.ts
+++ app.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "@tanstack/start/config";
 import viteTsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  server: {
+    preset: "cloudflare-pages",
+  },
   vite: {
     plugins: [viteTsConfigPaths({ projects: ["./tsconfig.json"] })],
   },
